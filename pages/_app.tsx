d6558f02ba6853ad1dede42f134de0e4c510bec1
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2021-09-11 12:43:50 +0200

    Add darkreader meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Provider session={pageProps.session}>
+    <Head>
+      <meta name="darkreader" content="594276ed-edc7-419b-86a7-c1e18fffcfa9" />
+    </Head>
     <Component {...pageProps} />
   </Provider>
 );
